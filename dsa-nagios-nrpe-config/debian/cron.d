6f1b9549915d9d7837e09a3a53054237801b573d
Peter Palfrader <peter@palfrader.org>
2008-07-06 11:50:32 +0200

    [project @ peter@palfrader.org-20080706095032-bstq0eycfjc6tbfi] No longer do @daily runs of dsa-update-apt-status - the hourly runs do the stuff if the last run was over a day ago

--- dsa-nagios-nrpe-config/debian/cron.d
+++ dsa-nagios-nrpe-config/debian/cron.d
@@ -1,8 +1,6 @@
 @reboot  root sleep 10m && [ -x /usr/share/dsa/weak-ssh-keys-check ] && /usr/share/dsa/weak-ssh-keys-check -s /var/cache/dsa/nagios/weak-ssh-keys
 @daily   root              [ -x /usr/share/dsa/weak-ssh-keys-check ] && /usr/share/dsa/weak-ssh-keys-check -s /var/cache/dsa/nagios/weak-ssh-keys
 
-@reboot  root [ -x /usr/sbin/dsa-update-apt-status ] && /usr/sbin/dsa-update-apt-status
-@daily   root [ -x /usr/sbin/dsa-update-apt-status ] && /usr/sbin/dsa-update-apt-status
-@hourly  root [ -x /usr/sbin/dsa-update-apt-status ] && /usr/sbin/dsa-update-apt-status hourly
+@hourly  root [ -x /usr/sbin/dsa-update-apt-status ] && /usr/sbin/dsa-update-apt-status
 
 # vim:ft=crontab:
