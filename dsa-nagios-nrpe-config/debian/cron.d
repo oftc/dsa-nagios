836aa8da019b59784544e577336a686c8291824f
Peter Palfrader <peter@palfrader.org>
2009-03-03 11:28:47 +0000

    [project @ peter@palfrader.org-20090303112847-9thng45h9jui32cj] run samhain check from cron

--- dsa-nagios-nrpe-config/debian/cron.d
+++ dsa-nagios-nrpe-config/debian/cron.d
@@ -2,5 +2,6 @@
 @daily   root              [ -x /usr/share/dsa/weak-ssh-keys-check ] && /usr/share/dsa/weak-ssh-keys-check -s /var/cache/dsa/nagios/weak-ssh-keys
 
 @hourly  root [ -x /usr/sbin/dsa-update-apt-status ] && /usr/sbin/dsa-update-apt-status
+13 */4 * * * root [ -x /usr/sbin/dsa-update-apt-samhain ] && /usr/sbin/dsa-update-apt-samhain
 
 # vim:ft=crontab:
