35cfb17ee615918cb3b1f00089f1d558d4e6e3a4
Peter Palfrader <peter@palfrader.org>
2009-03-03 20:43:11 +0000

    [project @ peter@palfrader.org-20090303204311-ej5qnljpoh3ejukk] fix crontab

--- dsa-nagios-nrpe-config/debian/cron.d
+++ dsa-nagios-nrpe-config/debian/cron.d
@@ -2,6 +2,6 @@
 @daily   root              [ -x /usr/share/dsa/weak-ssh-keys-check ] && /usr/share/dsa/weak-ssh-keys-check -s /var/cache/dsa/nagios/weak-ssh-keys
 
 @hourly  root [ -x /usr/sbin/dsa-update-apt-status ] && /usr/sbin/dsa-update-apt-status
-13 */4 * * * root [ -x /usr/sbin/dsa-update-apt-samhain ] && /usr/sbin/dsa-update-apt-samhain
+13 */4 * * * root [ -x /usr/sbin/dsa-update-samhain-status ] && /usr/sbin/dsa-update-samhain-status
 
 # vim:ft=crontab:
