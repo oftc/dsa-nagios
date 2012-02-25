2a4872e9c6a3a26ce90de5ef0f795ec8f96a279e
Peter Palfrader <peter@palfrader.org>
2012-02-25 13:30:03 +0100

    update cronjob time

--- dsa-nagios-checks/debian/cron.d
+++ dsa-nagios-checks/debian/cron.d
@@ -5,5 +5,5 @@
 13 */4 * * * root [ -x /usr/sbin/dsa-update-samhain-status ] && /usr/sbin/dsa-update-samhain-status
 
 SHELL=/bin/bash
-4 12 25 * *   root [ -x /usr/sbin/dsa-update-unowned-file-status ] && sleep $(( $RANDOM \% 86400 )) && /usr/sbin/dsa-update-unowned-file-status
+40 12 25 * *   root [ -x /usr/sbin/dsa-update-unowned-file-status ] && sleep $(( $RANDOM \% 86400 )) && /usr/sbin/dsa-update-unowned-file-status
 # vim:ft=crontab:
