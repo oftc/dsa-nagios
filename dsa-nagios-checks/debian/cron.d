b67d4bcbfed68f84be34c584964f1f2eeb07a867
Peter Palfrader <peter@palfrader.org>
2014-12-07 14:28:48 +0100

    Fix iptables-save cronjob

--- dsa-nagios-checks/debian/cron.d
+++ dsa-nagios-checks/debian/cron.d
@@ -1,6 +1,6 @@
 @hourly  root [ -x /usr/sbin/dsa-update-apt-status ] && /usr/sbin/dsa-update-apt-status 2>&1 | logger -t dsa-update-apt-status
-@hourly  root [ -x /sbin/iptables-save ] && umask 0177; /sbin/iptables-save | sed -e 's/\[.*//' -e 's/^#.*//' | sha256sum > /var/run/iptables-online.checksum
-@hourly  root [ -x /sbin/ip6tables-save ] && umask 0177; /sbin/ip6tables-save | sed -e 's/\[.*//' -e 's/^#.*//' | sha256sum > /var/run/ip6tables-online.checksum
+@hourly  root if [ -x /sbin/iptables-save ]; then umask 0177 && /sbin/iptables-save | sed -e 's/\[.*//' -e 's/^#.*//' | sha256sum > /var/run/iptables-online.checksum; fi
+@hourly  root if [ -x /sbin/ip6tables-save ]; then umask 0177 && /sbin/ip6tables-save | sed -e 's/\[.*//' -e 's/^#.*//' | sha256sum > /var/run/ip6tables-online.checksum; fi
 13 */4 * * * root [ -x /usr/sbin/dsa-update-samhain-status ] && /usr/sbin/dsa-update-samhain-status
 
 SHELL=/bin/bash
